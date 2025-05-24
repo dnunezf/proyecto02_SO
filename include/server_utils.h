9acd90e801e0c7aca60262b242a7026cf9fe41d0
dnunezf <david.a.nunez27@outlook.com>
2025-05-24 11:22:16 -0600

    Estructura inicial del proyecto

--- include/server_utils.h
+++ include/server_utils.h
@@ -1,4 +1,14 @@
 /*
+* PROYECTO PROGRAMADO 02
+* SISTEMAS OPERATIVOS
+* ESTUDIANTES:
+* Ignacio Rodríguez Ovares, 118940243
+* David Núñez Franco, 119080008
+* Sharon María Araya Ramírez, 402530296
+* CREATED 24/05/2025
+* */
+
+/*
 * Funciones auxiliares del servidor
 */
 
--- server/main.c
+++ server/main.c
@@ -1,3 +1,13 @@
+/*
+* PROYECTO PROGRAMADO 02
+* SISTEMAS OPERATIVOS
+* ESTUDIANTES:
+* Ignacio Rodríguez Ovares, 118940243
+* David Núñez Franco, 119080008
+* Sharon María Araya Ramírez, 402530296
+* CREATED 24/05/2025
+* */
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
