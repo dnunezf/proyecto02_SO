6840c36105539222033f25fc5bba7ae7db21d133
dnunezf <david.a.nunez27@outlook.com>
2025-06-12 15:03:22 -0600

    Cambio

--- cmake-build-debug/CMakeFiles/client_exec.dir/Client/statistics.c.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/client_exec.dir/Client/statistics.c.o: \
- /home/f0x/CLionProjects/proyecto02_SO/Client/statistics.c \
- /usr/include/stdc-predef.h
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/f0x/CLionProjects/proyecto02_SO
+# Install script for directory: /home/david/Proyecto 02 - S.O/http_multiversion_server
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -42,12 +42,6 @@ if(NOT DEFINED CMAKE_OBJDUMP)
   set(CMAKE_OBJDUMP "/usr/bin/objdump")
 endif()
 
-string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
-       "${CMAKE_INSTALL_MANIFEST_FILES}")
-if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/f0x/CLionProjects/proyecto02_SO/cmake-build-debug/install_local_manifest.txt"
-     "${CMAKE_INSTALL_MANIFEST_CONTENT}")
-endif()
 if(CMAKE_INSTALL_COMPONENT)
   if(CMAKE_INSTALL_COMPONENT MATCHES "^[a-zA-Z0-9_.+-]+$")
     set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
@@ -61,6 +55,8 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "/home/f0x/CLionProjects/proyecto02_SO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+  string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
+       "${CMAKE_INSTALL_MANIFEST_FILES}")
+  file(WRITE "/home/david/Proyecto 02 - S.O/http_multiversion_server/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
